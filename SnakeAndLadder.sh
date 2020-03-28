cdace2ea09a1d0c34338e9c3e3b1d05f83155f16
dvranga <devangmranganth>
2020-03-28 13:03:41 +0530

    [ranganath]Updated:play the game till wining index 100

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -1,8 +1,25 @@
+
 # welcome to snake and ladder game simulator
 START_POSITION=0;
 NUMBER_OF_PLAYER=1;
-echo "start position = "$START_POSITION;
-echo "number of player= "$NUMBER_OF_PLAYER;
 #player rolls the die to get Random number between 1 to 6
 RANDOM_DIE_NUMBER=$(( $RANDOM%6+1 ))
-echo "random die number=" $RANDOM_DIE_NUMBER
+firstPlayerIndex=$START_POSITION;
+echo "firstPlayerIndex :"$START_POSITION;
+echo "NUMBER_OF_PLAYER :" $NUMBER_OF_PLAYER
+NOPLAY=0;
+LADDER=1;
+SNAKE=2;
+echo "select noplay,ladder or snake"
+SELECT=$(( $RANDOM%3 ))
+        case $SELECT in
+        $NOPLAY)
+                firstPlayerIndex=$firstPlayerIndex
+                echo noplay is selected ;;
+        $LADDER)
+                firstPlayerIndex=$(( $firstPlayerIndex + $RANDOM_DIE_NUMBER ))
+                echo ladder is selected ;;
+        $SNAKE)
+                firstPlayerIndex=$(( $firstPlayerIndex - $RANDOM_DIE_NUMBER ))
+                echo snake is selected ;;
+        esac
