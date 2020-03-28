73d956ab3345d1769718fcc265254659fa89c64d
dvranga <devangmranganth>
2020-03-28 11:56:29 +0530

    [ranganth]Added:generating random number by rooling die

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -1,5 +1,8 @@
 # welcome to snake and ladder game simulator
 START_POSITION=0;
 NUMBER_OF_PLAYER=1;
-echo $START_POSITION=0;
-echo $NUMBER_OF_PLAYER=1;
+echo "start position = "$START_POSITION;
+echo "number of player= "$NUMBER_OF_PLAYER;
+#player rolls the die to get Random number between 1 to 6
+RANDOM_DIE_NUMBER=$(( $RANDOM%6+1 ))
+echo "random die number=" $RANDOM_DIE_NUMBER
