75630bea15d07b28b16a463c997aabab94c628f2
dvranga <devangmranganth>
2020-03-28 13:34:10 +0530

    [ranganaht] Update:game play till win and if position is zere initialize zero

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -1,8 +1,37 @@
 # welcome to snake and ladder game simulator
 START_POSITION=0;
 NUMBER_OF_PLAYER=1;
-echo "start position = "$START_POSITION;
-echo "number of player= "$NUMBER_OF_PLAYER;
 #player rolls the die to get Random number between 1 to 6
-RANDOM_DIE_NUMBER=$(( $RANDOM%6+1 ))
-echo "random die number=" $RANDOM_DIE_NUMBER
+firstPlayerIndex=$START_POSITION;
+echo "firstPlayerIndex :"$START_POSITION;
+echo "NUMBER_OF_PLAYER :" $NUMBER_OF_PLAYER
+NOPLAY=0;
+LADDER=1;
+SNAKE=2;
+echo "select noplay,ladder or snake"
+WIN_POSTION=100;
+echo "winning position is: " $WIN_POSTION
+while [ $firstPlayerIndex -le $WIN_POSTION ]
+do
+	SELECT=$(( $RANDOM%3 ))
+	RANDOM_DIE_NUMBER=$(( $RANDOM%6+1 ))
+        case $SELECT in
+        $NOPLAY)
+                firstPlayerIndex=$firstPlayerIndex
+                echo "(Noplay)position is : " $firstPlayerIndex ;;
+        $LADDER)
+                firstPlayerIndex=$(( $firstPlayerIndex + $RANDOM_DIE_NUMBER ))
+                echo "(ladder) position is : " $firstPlayerIndex ;;
+        $SNAKE)
+                firstPlayerIndex=$(( $firstPlayerIndex - $RANDOM_DIE_NUMBER ))
+		echo "(snake)position is : " $firstPlayerIndex
+		if [ $firstPlayerIndex -lt $START_POSITION ]
+		then
+			echo "firstPlayerindex " $firstPlayerIndex
+			echo "firstPlayerindex is less than zero so initialize zero"
+			firstPlayerIndex=$START_POSITION
+			echo $firstPlayerIndex
+		fi
+		;;
+        esac
+done
